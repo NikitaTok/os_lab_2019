fed5b68654053858bdc3243509c2479cddf582fe
NikitaTok <nek.tokar20466@mail.ru>
2024-10-02 16:17:37 +0000

    lab2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,15 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	int l = strlen(str);
+    
+	for (int i=0, j=l-1; i<j; i++, j--)
+	{
+        char one = str[i];
+        str[i] = str[j];
+        str[j] = one;
+    }
 }
 
--- lab2/src/swap/swap.c
+++ lab2/src/swap/swap.c
@@ -2,5 +2,7 @@
 
 void Swap(char *left, char *right)
 {
-	// ваш код здесь
+	char one = *left;
+	*left = *right;
+	*right = one;
 }
